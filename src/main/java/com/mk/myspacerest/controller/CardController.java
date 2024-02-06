64d5d879ee89d540b823e198b5237712ae54007b
mkizhevsk <mkizhevsk@gmail.com>
2024-02-06 15:09:38 +0400

    some changes

---
+++ src/main/java/com/mk/myspacerest/controller/CardController.java
@@ -0,0 +1,11 @@
+package com.mk.myspacerest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class CardController {
+
+
+}
