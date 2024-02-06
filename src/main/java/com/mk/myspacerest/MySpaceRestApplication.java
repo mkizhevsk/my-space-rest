fd677b1f3b8588862a41a64fd72ac770ab9c61c5
mkizhevsk <mkizhevsk@gmail.com>
2024-02-06 15:04:57 +0400

    first commit

---
+++ src/main/java/com/mk/myspacerest/MySpaceRestApplication.java
@@ -0,0 +1,13 @@
+package com.mk.myspacerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpaceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpaceRestApplication.class, args);
+	}
+
+}
