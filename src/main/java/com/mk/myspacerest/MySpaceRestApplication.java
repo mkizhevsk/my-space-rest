eed8d85fd966531bfaad5a8eb0a1225893b3c29f
mkizhevsk <mkizhevsk@gmail.com>
2024-07-15 18:06:20 +0400

    first version

--- src/main/java/com/mk/myspacerest/MySpaceRestApplication.java
+++ src/main/java/com/mk/myspacerest/MySpaceRestApplication.java
@@ -1,8 +1,11 @@
 package com.mk.myspacerest;
 
+import com.mk.myspacerest.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class MySpaceRestApplication {
 
