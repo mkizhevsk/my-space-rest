2b80fbb7d1c35566662f39cef7f55613fa3be5c7
mkizhevsk <mkizhevsk@gmail.com>
2024-07-26 17:48:14 +0400

    some changes

--- src/main/java/com/mk/myspacerest/controller/AuthController.java
+++ src/main/java/com/mk/myspacerest/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -25,5 +26,13 @@ public class AuthController {
         String token = tokenService.generateToken(authentication);
         LOG.info("Token granted {}", token);
         return token;
+    }
+
+    @PostMapping("/refresh-token")
+    public String refreshToken(@RequestHeader("Authorization") String authorizationHeader) {
+        String token = authorizationHeader.replace("Bearer ", "");
+        String newToken = tokenService.refreshToken(token);
+        LOG.info("Token refreshed {}", newToken);
+        return newToken;
     }
 }
--- src/main/java/com/mk/myspacerest/service/TokenService.java
+++ src/main/java/com/mk/myspacerest/service/TokenService.java
@@ -1,24 +1,27 @@
 package com.mk.myspacerest.service;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Service
 public class TokenService {
 
     private final JwtEncoder encoder;
+    private final JwtDecoder decoder;
 
-
-    public TokenService(JwtEncoder encoder) {
+    public TokenService(JwtEncoder encoder, JwtDecoder decoder) {
         this.encoder = encoder;
+        this.decoder = decoder;
     }
 
     public String generateToken(Authentication authentication) {
@@ -34,6 +37,20 @@ public class TokenService {
                 .claim("scope", scope)
                 .build();
         return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    public String refreshToken(String token) {
+        Jwt decodedToken = decoder.decode(token);
+        String username = decodedToken.getSubject();
+        String scope = decodedToken.getClaim("scope");
+
+        // Create a new Authentication object
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null,
+                scope == null ? Collections.emptyList() : Arrays.stream(scope.split(" "))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList()));
+
+        return generateToken(authentication);
     }
 
 }
