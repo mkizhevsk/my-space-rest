916a116e3a72d998ee84fc53d33394bc12f566d2
mkizhevsk <mkizhevsk@gmail.com>
2024-08-13 16:29:31 +0400

    some changes

---
+++ src/main/java/com/mk/myspacerest/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.mk.myspacerest.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private int errorCode;
+    private String message;
+
+}
