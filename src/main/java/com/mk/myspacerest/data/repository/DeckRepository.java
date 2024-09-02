94e2d1745cffc4d7de3107c517219d7141f0e488
mkizhevsk <mkizhevsk@gmail.com>
2024-09-02 10:16:24 +0400

    deck added

---
+++ src/main/java/com/mk/myspacerest/data/repository/DeckRepository.java
@@ -0,0 +1,16 @@
+package com.mk.myspacerest.data.repository;
+
+import com.mk.myspacerest.data.entity.Deck;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeckRepository extends CrudRepository<Deck, Integer> {
+
+    @Query(value = "SELECT * FROM deck d WHERE d.username = ?1",
+            nativeQuery = true)
+    List<Deck> getDecksByUser(String username);
+}
